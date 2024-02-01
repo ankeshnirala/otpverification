9a651af52fe572605a9e4c55646abaf9fc36b0cf
ankeshnirala <ankeshnirala@gmail.com>
2024-02-01 08:10:12 +0000

    twilio sms integration done

--- api/handler.go
+++ api/handler.go
@@ -1,1 +1,61 @@
 package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/ankeshnirala/otpverification/data"
+	"github.com/gin-gonic/gin"
+)
+
+var appTimeout = 10 * time.Second
+
+func (app *Config) SendOTP() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		_, cancel := context.WithTimeout(context.Background(), appTimeout)
+		var payload data.OTPData
+		defer cancel()
+
+		app.validateBody(ctx, &payload)
+
+		newData := data.OTPData{
+			PhoneNumber: payload.PhoneNumber,
+		}
+
+		_, err := app.twilioSendOTP(newData.PhoneNumber)
+		if err != nil {
+			fmt.Println("Error: ", err)
+			app.errorJSON(ctx, err)
+			return
+		}
+
+		app.writeJSON(ctx, http.StatusAccepted, "OTP sent succesfully")
+	}
+}
+
+func (app *Config) VerifyOTP() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		_, cancel := context.WithTimeout(context.Background(), appTimeout)
+		var payload data.VerifyData
+		defer cancel()
+
+		app.validateBody(ctx, &payload)
+
+		newData := data.VerifyData{
+			User: payload.User,
+			Code: payload.Code,
+		}
+
+		err := app.twilioVerifyOTP(newData.User.PhoneNumber, newData.Code)
+		if err != nil {
+			fmt.Println("Error: ", err)
+			app.errorJSON(ctx, err)
+			return
+		}
+
+		app.writeJSON(ctx, http.StatusAccepted, "OTP verified succesfully")
+
+	}
+}
--- api/service.go
+++ api/service.go
@@ -1,1 +1,46 @@
 package api
+
+import (
+	"errors"
+
+	"github.com/twilio/twilio-go"
+
+	twilioApi "github.com/twilio/twilio-go/rest/verify/v2"
+)
+
+var client *twilio.RestClient = twilio.NewRestClientWithParams(twilio.ClientParams{
+	Username: envAccountSID(),
+	Password: envAuthToken(),
+})
+
+func (app *Config) twilioSendOTP(phoneNumber string) (string, error) {
+	params := &twilioApi.CreateVerificationParams{}
+
+	params.SetTo(phoneNumber)
+	params.SetChannel("sms")
+
+	response, err := client.VerifyV2.CreateVerification(envServiceSID(), params)
+	if err != nil {
+		return "", err
+	}
+
+	return *response.Sid, nil
+}
+
+func (app *Config) twilioVerifyOTP(phoneNumber string, code string) error {
+	params := &twilioApi.CreateVerificationCheckParams{}
+
+	params.SetTo(phoneNumber)
+	params.SetCode(code)
+
+	response, err := client.VerifyV2.CreateVerificationCheck(envServiceSID(), params)
+	if err != nil {
+		return err
+	}
+
+	if *response.Status != "approved" {
+		return errors.New("not a valid code")
+	}
+
+	return nil
+}
